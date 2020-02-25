d46b700f356d7717a80f49bd1c96a5bd2f81cfee
Erum <erummushtaq@polonius.usc.edu>
2020-02-25 09:54:43 -0800

    Code cleaned and comments added

--- Demo_File.m
+++ Demo_File.m
@@ -10,63 +10,41 @@ ns = 4; % Size of sniffer Coil
 %% Calibration Phase
 % Interference Signals 
 %wI(1,:) = fft(BPSK(127));
-wI(1,:) = fft(bpsk_new([1 0 1 1 0], 128));
-% wI(1,:) = 17000;
+wI(1,:) = fft(bpsk_new([1 1 0 0 0], 128));  % bpsk_new(data_stream, fc)
+
 % Channels H(w) & G(w)
- si = size(wI,1); 
+ si = size(wI,1); % number of interferes
  wH = randn(nc, si)*0.2 + i*randn(nc, si)*0.2;
  wG = randn(ns, si)*0.2 + i*randn(ns, si)*0.2;
- 
-%  wS = wG * wI(:,7);
-%  wR = wH * wI(:,7);
 
- wS = wG * 16000;
- wR = wH * 16000;
+ wS = wG * wI(:,50);
+ wR = wH * wI(:,50);
 
-% Transform
+% Compute Transform
  TLeft_inverse = (inv(wS*wS')* wS * wR')';
  TPseudo_inv = (pinv(wS')*wR')';
  TPatent_T = wH * inv(wG' * wG ) * wG';
+ 
+%% Acquisition Phase
 fc = 200;
  for kx=1:1:nvx
 
-   wI(1,:) = fft(bpsk_new([1 0 1 1 0], 700));
-   fc = fc+100;
+   wI(1,:) = fft(bpsk_new([1 0 1 1 0], fc)); % change frequency on every itertion
+   fc = fc+20;
    for ky=1:1:nvy
+       
  %% Define Signals
  %% M(w)
  wM(1:1:nc) = fft(st(kx,ky,:));
- 
- %% Interference Signals 
-
-% wI(1,:) = fft(BPSK(20));
-%  plot(abs(wI(1,:)))
-% wI(2,:) = fft(bpsk([1 0 1 1 0], 128));
-% wI(3,:) = fft(bpsk([1 0 1 1 0], 126.5));
-% wI(4,:) = fft(bpsk([1 0 1 1 0], 127.5));
-% wI(5,:) = fft(bpsk([1 0 1 1 0], 125.5));
-% wI(6,:) = fft(bpsk([1 0 1 1 0], 128.5));
- 
- %% H(w) & G(w)
- si = size(wI,1); 
-%  wH = randn(nc, si)*0.2 + i*randn(nc, si)*0.2;
-%  wG = randn(ns, si)*0.2 + i*randn(ns, si)*0.2;
- 
-  wR = wM' + wH * wI(:,kx);
-% if ky > 100 && ky << 150
-%  wR = wM' + wH * wI(:,kx);
-% else 
-%  wR = wM';
-% end
-% figure;plot(abs(fft(st(:,128,1))));
-% hold on;
-% plot(abs(wI(1,1:256)));
-% figure;subplot(1,3,1);plot(abs(wM'));
-% subplot(1,3,2);plot(abs(wI));
-% subplot(1,3,3);plot(abs(wR));
+ wR = wM' + wH * wI(:,kx);
  wS = wG * wI(:,kx);
+ %% Plot signals
+% figure;subplot(1,3,1);plot(abs(fft(st(128,:,1)))); axis square; title('M(f)'); % 128 TR
+% subplot(1,3,2);plot(abs(wI(1,1:1:256))); axis square; title('I(f)'); % 128 TR
+% subplot(1,3,3);plot(abs((fft(st(128,:,1)))+(wI(1,1:1:256)))); axis square; title('R(f)');
+ 
 
- %% Estimate Mw
+ %% Estimate Mw using transform (from calibration Phase)
  e_Left_inverse = wR - TLeft_inverse*wS;
  e_TPseudo_inv = wR - TPseudo_inv*wS;
  e_TPatent_T = wR - TPatent_T*wS;
@@ -81,9 +59,7 @@ fc = 200;
  kspace_PI(kx,ky,:) = k_TPseudo_inv;
  kspace_PT(kx,ky,:) = k_TPatent_T;
  kspace_wR(kx,ky,:) = k_TR;
-% wM' - wMe
-% wM' - wwMe
-% wM' - wwwMe 
+
  
    end 
  end
@@ -93,7 +69,10 @@ fc = 200;
  I_PT = ifft2c(kspace_PT); % nx, ny, nc, nt
  I_WI = ifft2c(st);
  
+  %% Error
+% error = I_wI - I_PI;
  %% Coil Combination
+%  Sum of squares
 %  rSoS = sqrt(sum(abs(fftshift(ifft2(ifftshift(recon)))).^2,3));
 %  rSoS = sqrt(sum(abs(fftshift(ifft2(ifftshift(recon)))).^2,3));
 %  rSoS = sqrt(sum(abs(fftshift(ifft2(ifftshift(recon)))).^2,3));
